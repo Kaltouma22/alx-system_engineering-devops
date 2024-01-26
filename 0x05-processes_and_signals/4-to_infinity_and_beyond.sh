f27bce65fe38f27f61eb8b300f5076930f1f04be
Kaltouma <kchbiki363@gmail.com>
2024-01-26 22:50:56 +0100

    done

--- 0x05-processes_and_signals/4-to_infinity_and_beyond.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Display "To infinity and beyond" indefinitely with a 2-second delay
-trap 'echo "Terminated"; exit' INT
-
-while true; do
-    echo "To infinity and beyond"
-    sleep 2
-done
