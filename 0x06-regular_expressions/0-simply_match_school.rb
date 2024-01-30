cdf26912745769f4f9bb6c11679e4e49192ebedd
Kaltouma <kchbiki363@gmail.com>
2024-01-30 23:51:39 +0100

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
-#!/usr/bin/ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{__FILE__} <string>"
-  exit 1
-end
-
-# Get the argument
-input_string = ARGV[0]
-
-# Define the regular expression
-regexp = /School/
-
-# Check if the input matches the regular expression
-if match_data = input_string.match(regexp)
-  puts "#{match_data[0]}$"
-else
-  puts "$"
-end
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/School/).join
